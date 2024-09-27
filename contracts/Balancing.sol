aebdd7ee66a87b7b82deeecc9f38155262ddbd26
Vincent de Wit <Vincent.j.wit@gmail.com>
2024-09-27 11:33:12 +0200

    Update Balancing.sol
    weird indentations

--- contracts/Balancing.sol
+++ contracts/Balancing.sol
@@ -53,31 +53,31 @@ contract Balancing {
         issueTBD = '0';
     }
 
-// create functon to initialize contract. create access right for admin to set issue and read rights of the discourse.
+    // create functon to initialize contract. create access right for admin to set issue and read rights of the discourse.
 
-	function convertToString(uint256 value) internal pure returns (string memory) {
-		if (value == 0) {
-			return "0";
-		}
-
-		uint256 temp = value;
-		uint256 digits;
-
-		while (temp != 0) {
-			digits++;
-			temp /= 10;
-		}
-
-		bytes memory buffer = new bytes(digits);
-
-		while (value != 0) {
-			digits--;
-			buffer[digits] = bytes1(uint8(48 + (value % 10)));
-			value /= 10;
-		}
-
-		return string(buffer);
+    function convertToString(uint256 value) internal pure returns (string memory) {
+	if (value == 0) {
+		return "0";
 	}
+
+	uint256 temp = value;
+	uint256 digits;
+
+	while (temp != 0) {
+		digits++;
+		temp /= 10;
+	}
+
+	bytes memory buffer = new bytes(digits);
+
+	while (value != 0) {
+		digits--;
+		buffer[digits] = bytes1(uint8(48 + (value % 10)));
+		value /= 10;
+	}
+
+	return string(buffer);
+    }
 
     function compareStrings(string memory a, string memory b) public view returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
