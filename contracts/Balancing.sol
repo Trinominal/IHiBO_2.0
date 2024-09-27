3b2a2fff63034d8c6dc5b057ac216a0ef6446896
Quibus <Vincent.j.wit@gmail.com>
2024-09-27 11:45:08 +0200

    indentation problems

--- contracts/Balancing.sol
+++ contracts/Balancing.sol
@@ -56,25 +56,28 @@ contract Balancing {
     // create functon to initialize contract. create access right for admin to set issue and read rights of the discourse.
 
     function convertToString(uint256 value) internal pure returns (string memory) {
-	if (value == 0) {
-		return "0";
-	}
+        if (value == 0) {
+            return "0";
+        }
 
-	uint256 temp = value;
-	uint256 digits;
+        uint256 temp = value;
+        uint256 digits;
 
-	while (temp != 0) {
-		digits++;
-		temp /= 10;
-	}
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
 
-	bytes memory buffer = new bytes(digits);
+        bytes memory buffer = new bytes(digits);
 
-	while (value != 0) {
-		digits--;
-		buffer[digits] = bytes1(uint8(48 + (value % 10)));
-		value /= 10;
-	}
+        while (value != 0) {
+            digits--;
+            buffer[digits] = bytes1(uint8(48 + (value % 10)));
+            value /= 10;
+        }
+
+        return string(buffer);
+    }
 
 	return string(buffer);
     }
