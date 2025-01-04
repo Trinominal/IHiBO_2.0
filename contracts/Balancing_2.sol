bd931170c926824b1360d8e57b1fde992d065752
Quibus <Vincent.j.wit@gmail.com>
2025-01-04 18:55:20 +0100

    work on bal-2
    functions for voting, and balancing
    not tested yet

--- contracts/Balancing_2.sol
+++ contracts/Balancing_2.sol
@@ -11,9 +11,9 @@ contract Balancing {
     using HitchensUnorderedKeySetLib for HitchensUnorderedKeySetLib.Set;
 
     struct Node {
-        uint256 magnitude;
+        uint256 weight;
         string polarity;
-        string metadata;
+        string justifications;
     }
 
     struct Graph {
@@ -26,52 +26,205 @@ contract Balancing {
     HitchensUnorderedKeySetLib.Set graphsIDs;
     mapping(uint256 => Graph) graphs;
 
-    mapping(address => HitchensUnorderedKeySetLib.Set) agentsReasons;
+    mapping(address => HitchensUnorderedKeySetLib.Set) sourcesReasons;//agentsArguments, sources
 
-    event OutputDecision(Graph graph);
+    event OutputDecision(Graph graph);// maybe instead output a hash or index for privacy
+    // then allow access only to particular agents.
 
 
     constructor() public {
-
         graphsIDs.insert(bytes32(uint256(1)));
+        Graph storage graph = graphs[1];
+        graph.issue = '0';
+    }
 
+    function setIssue(string memory issue)
+    	public
+    {
+    	// TODO change to depend on access rights and can only be called at the start
+    	if(true) {
+            Graph storage graph = graphs[1];
+    	    graph.issue = issue;
+    	}
+    }
+    
+    // is this function really needed?
+    function compareStrings(string memory a, string memory b) public view returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function voteOnReason
+    (
+        string memory justification, 
+        // string memory issue, 
+        string memory polarity, 
+        uint256 confidence
+    ) 
+        public
+        returns (int256 re)
+    {
+        Graph storage graph = graphs[1];
+
+        re = 0;
+        // check that reason is not known yet.
+        for (uint256 j = 1; j < graph.reasonsIDs.count() + 1; j++) {
+            if (compareStrings(graph.reasons[j].justification, justification) && 
+            // compareStrings(graph.reasons[j].issue, issue) && 
+            compareStrings(graph.reasons[j].polarity, polarity)) {
+                // conclude reason is already in reasons
+                // increase weight by magnitude
+                graph.reasons[j].weight = graph.reasons[j].weight + confidence*reputations[msg.sender];
+
+                HitchensUnorderedKeySetLib.Set storage source = sources[
+                    msg.sender
+                ];
+                source.insert(bytes32(j));
+                re = int256(j);
+                break;
+            }
+        }
+
+        if (re == 0) {// reason is new add reason and set weight to 1
+            uint256 reasonID = graph.reasonsIDs.count() + 1;
+            graph.reasonsIDs.insert(bytes32(reasonID));
+            Reason storage reason = graph.reasons[reasonID];
+            // reason.justification = justification;
+            graph.reasons[reasonID].justification = justification;
+            // reason.issue = issue;
+            // reason.polarity = polarity;
+            graph.reasons[reasonID].polarity = polarity;
+            // weights[reasonID-1] = confidence*reputations[msg.sender];
+            graph.reasons[reasonID].weight = graph.reasons[reasonID].weight + confidence*reputations[msg.sender];
+    
+            HitchensUnorderedKeySetLib.Set storage source = sources[
+                msg.sender
+            ];
+            source.insert(bytes32(reasonID));
+            re = int256(reasonID);
+        }
     }
 
 
+    // probably this can be done in a nicer way maybe via a library
+    function convertToString(uint256 value) 
+        internal 
+        pure 
+        returns (string memory) 
+    {
+        if (value == 0) {
+            return "0";
+        }
 
-    // // create functon to initialize contract. create access right for admin to set issue and read rights of the discourse.
+        uint256 temp = value;
+        uint256 digits;
 
-    // function convertToString(uint256 value) 
-    //     internal 
-    //     pure 
-    //     returns (string memory) 
-    // {
-    //     if (value == 0) {
-    //         return "0";
-    //     }
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
 
-    //     uint256 temp = value;
-    //     uint256 digits;
+        bytes memory buffer = new bytes(digits);
 
-    //     while (temp != 0) {
-    //         digits++;
-    //         temp /= 10;
-    //     }
+        while (value != 0) {
+            digits--;
+            buffer[digits] = bytes1(uint8(48 + (value % 10)));
+            value /= 10;
+        }
 
-    //     bytes memory buffer = new bytes(digits);
+        return string(buffer);
+    }
 
-    //     while (value != 0) {
-    //         digits--;
-    //         buffer[digits] = bytes1(uint8(48 + (value % 10)));
-    //         value /= 10;
-    //     }
+    function procedureAdditive()
+        public
+        // view 
+        returns (string memory dec)
+    {
+        Graph storage graph = graphs[1];
 
-    //     return string(buffer);
-    // }
+        dec = '';
+        uint256 magn = 0;
+        uint256 pos = 0;
+        uint256 ppos = 0;
+        uint256 neg = 0;
+        uint256 nneg = 0;
+        int256 sum = 0;
+        uint256 neut = 0;
+        string memory cs = '';
+        uint256 rs = graph.reasonsIDs.count();   
 
-    // function compareStrings(string memory a, string memory b) public view returns (bool) {
-    //     return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-    // }
+
+        for (uint256 i = 0; i < rs; i++){
+                
+            uint256 reasonID = uint256(graph.reasonsIDs.keyAtIndex(i));
+            Reason storage reason = graph.reasons[reasonId];//not sure if this works
+            
+            cs = string(abi.encodePacked(cs, " ", i, " ", reason.justification));
+
+            if (compareStrings(reason.issue, issueTBD)) {
+                if (compareStrings(reason.polarity, '0')) { 
+                    neut += uint256(reason.weight);
+                }
+                else if (compareStrings(reason.polarity, '-')) { 
+                    neg += uint256(reason.weight);
+                }
+                else if (compareStrings(reason.polarity, '+')) { 
+                    pos += uint256(reason.weight);
+                }
+                else if (compareStrings(reason.polarity, '--')) { 
+                    nneg += uint256(reason.weight);
+                }
+                else if (compareStrings(reason.polarity, '++')) { 
+                    ppos += uint256(reason.weight);
+                }
+            }
+        }  
+
+        sum = int256(pos) + 2*int256(ppos) - (int256(neg) + 2*int256(nneg));
+        magn = sum < 0 ? uint256(-sum) : uint256(sum);
+
+        if (!(2*magn > neut)) {
+            magn = neut;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '0', ')'));
+        }
+        else if (sum > 0 && ppos > pos) {
+            magn = ppos;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '++', ')'));
+        }
+        else if (sum > 0) {
+            magn = pos;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '+', ')'));
+        }
+        else if (sum < 0 && nneg > neg) {
+            magn = nneg;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '--', ')'));
+        }
+        else if (sum < 0) {
+            magn = neg;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '-', ')'));
+        }
+        else {
+            magn = 0;
+            string memory Magn = convertToString(magn);
+            dec = string(abi.encodePacked(dec, '(', Magn, ', ', '0', ')'));
+        }
+
+
+        graph.decision = dec;
+        emit Output(graph);
+
+
+        return dec;
+    }
+
+    // // create function to initialize contract. create access right for admin to set issue and read rights of the discourse.
+
+ 
+
 
     // function setReputation(uint256 rep, address subject) 
     //     public
@@ -121,14 +274,6 @@ contract Balancing {
     //     return issueTBD;
     // }
     
-    // function setIssue(string memory issue)
-    // 	public
-    // {
-    // 	// TODO change to depend on access rights and can only be called at the start
-    // 	if(true) {
-    // 	    issueTBD = issue;
-    // 	}
-    // }
 
     // function getReasons()
     //     public
@@ -153,170 +298,6 @@ contract Balancing {
     //         issues[i] = reason.issue;
     //         polarities[i] = reason.polarity;
     //     }
-    // }
-
-    // function voteOnReason
-    // (
-    //     string memory justification, 
-    //     string memory issue, 
-    //     string memory polarity, 
-    //     uint256 magnitude
-    // ) 
-    //     public
-    //     returns (int256 re)
-    // {
-
-    //     re = 0;
-    //     // check that reason is not known yet.
-    //     for (uint256 j = 1; j < reasonsIds.count() + 1; j++) {
-    //         if (compareStrings(reasons[j].justification, justification) && 
-    //         compareStrings(reasons[j].issue, issue) && 
-    //         compareStrings(reasons[j].polarity, polarity)) {
-    //             // conclude reason is already in reasons
-    //             // increase weight by +1
-    //             weights[j-1] = weights[j-1] + magnitude*reputations[msg.sender];
-    //             re = 1;
-    //             break;
-    //         }
-    //     }
-
-    //     if (re == 0) {// reason is new add reason and set weight to 1
-    //         uint256 reasonID = reasonsIds.count() + 1;
-    //         reasonsIds.insert(bytes32(reasonID));
-    //         Reason storage reason = reasons[reasonID];
-    //         reason.justification = justification;
-    //         reason.issue = issue;
-    //         reason.polarity = polarity;
-    //         weights[reasonID-1] = magnitude*reputations[msg.sender];
-    
-    //         HitchensUnorderedKeySetLib.Set storage source = sources[
-    //             msg.sender
-    //         ];
-    //         source.insert(bytes32(reasonID));
-    //         re = int256(reasonID);
-    //     }
-    // }
-   
-
-    // function procedureAdditive()
-    //     public
-    //     // view 
-    //     returns (string memory dec)
-    // {
-
-    //     dec = '';
-    //     uint256 magn = 0;
-    //     uint256 pos = 0;
-    //     uint256 ppos = 0;
-    //     uint256 neg = 0;
-    //     uint256 nneg = 0;
-    //     int256 sum = 0;
-    //     uint256 neut = 0;
-    //     string memory cs = '';
-    //     uint256 rs = reasonsIds.count();   
-
-
-    //     for (uint256 i = 0; i < rs; i++){
-                
-    //         uint256 reasonId = uint256(reasonsIds.keyAtIndex(i));
-    //         Reason storage reason = reasons[reasonId];
-            
-    //         cs = string(abi.encodePacked(cs, " ", i, " ", reason.justification));
-
-    //         if (compareStrings(reason.issue, issueTBD)) {
-    //             if (compareStrings(reason.polarity, '0')) { 
-    //                 neut += uint256(weights[i]);
-    //             }
-    //             else if (compareStrings(reason.polarity, '-')) { 
-    //                 neg += uint256(weights[i]);
-    //             }
-    //             else if (compareStrings(reason.polarity, '+')) { 
-    //                 pos += uint256(weights[i]);
-    //             }
-    //             else if (compareStrings(reason.polarity, '--')) { 
-    //                 nneg += uint256(weights[i]);
-    //             }
-    //             else if (compareStrings(reason.polarity, '++')) { 
-    //                 ppos += uint256(weights[i]);
-    //             }
-    //         }
-    //     }  
-
-    //     sum = int256(pos) + 2*int256(ppos) - (int256(neg) + 2*int256(nneg));
-    //     magn = sum < 0 ? uint256(-sum) : uint256(sum);
-
-    //     // pos += 2*ppos;
-    //     // neg += 2*nneg;
-    //     // magn = pos > neg ? pos - neg : neg - pos;
-    //     // magn = 2*magn < neut ? neut-magn : (2*magn-neut)/2;
-    //     // string memory Magn = convertToString(magn);
-
-    //     if (!(2*magn > neut)) {
-    //         magn = neut;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '0', ')'));
-    //         // dec = string(magn + '0');
-    //         // dec = '0';
-    //     }
-    //     else if (sum > 0 && ppos > pos) {
-    //         // 2*pos > 2*(neut+2*neg)) {
-    //         magn = ppos;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '++', ')'));
-    //         // dec = string(magn + '++');
-    //         // dec = '++';
-    //     }
-    //     else if (sum > 0) {
-    //         // 2*pos > neut+2*neg) {
-    //         magn = pos;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '+', ')'));
-    //         // dec = string(magn + '+');
-    //         // dec = '+';
-    //     }
-    //     else if (sum < 0 && nneg > neg) {
-    //         // 2*neg > 2*(neut+2*pos)) {
-    //         magn = nneg;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '--', ')'));
-    //         // dec = string(magn + '--');
-    //         // dec = '--';
-    //     }
-    //     else if (sum < 0) {
-    //         // 2*neg > neut+2*pos) {
-    //         magn = neg;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '-', ')'));
-    //         // dec = string(magn + '-');
-    //         // dec = '-';
-    //     }
-    //     else {
-    //         magn = 0;
-    //         string memory Magn = convertToString(magn);
-    //         dec = string(abi.encodePacked(dec, '(', Magn, ', ', '0', ')'));
-    //     }
-
-
-    // // untested code for emiting decisionKey
-    // // /*
-    //     // contextTBD.rcount = rs;
-    //     // contextTBD.reasons = reasons;
-    //     // contextTBD.issue = issueTBD;
-
-    //     // uint256 decisionID = decisionsIds.count() + 1;
-    //     // decisionsIds.insert(bytes32(decisionID));
-    //     // Decision storage decision = decisions[decisionID];
-    //     // decision.context = contextTBD;
-    //     // decision.weights = weights;
-    //     // decision.outputPolarity = uint256(sum);
-
-    //     // emit Output(decisionID);
-    //     // */
-
-    //     // emit Output(42); // ^^'
-    //     emit Output(cs ,issueTBD, dec);
-
-    //     return dec;
     // }
 
 }
