df463fb5dd81172eea9b6f34ee079eb1456b66ee
Vincent de Wit <Vincent.j.wit@gmail.com>
2025-04-13 00:59:55 +0200

    update to names and removing some bugs

--- contracts/Balancing_2.sol
+++ contracts/Balancing_2.sol
@@ -10,41 +10,41 @@ import "./HitchensUnorderedKeySet.sol";
 contract Balancing {
     using HitchensUnorderedKeySetLib for HitchensUnorderedKeySetLib.Set;
 
-    struct Node {
+    struct Reason {
         uint256 weight;
         string polarity;
-        string justifications;
+        string ground;
     }
 
-    struct Graph {
+    struct Weighing {
         HitchensUnorderedKeySetLib.Set reasonsIDs;
-        mapping(uint256 => Node) reasons;
-        string issue;
-        string decision;
+        mapping(uint256 => Reason) reasons;
+        string option;
+        string output;
     }
 
-    HitchensUnorderedKeySetLib.Set graphsIDs;
-    mapping(uint256 => Graph) graphs;
+    HitchensUnorderedKeySetLib.Set weighingIDs;
+    mapping(uint256 => Weighing) weighings;
 
     mapping(address => HitchensUnorderedKeySetLib.Set) sourcesReasons;//agentsArguments, sources
 
-    event OutputDecision(Graph graph);// maybe instead output a hash or index for privacy
+    event OutputDecision(Weighing weighing);// maybe instead output a hash or index for privacy
     // then allow access only to particular agents.
 
 
     constructor() public {
-        graphsIDs.insert(bytes32(uint256(1)));
-        Graph storage graph = graphs[1];
-        graph.issue = '0';
+        weighingsIDs.insert(bytes32(uint256(1)));
+        Weighing storage weighing = weighings[1];
+        weighing.option = '0';
     }
 
-    function setIssue(string memory issue)
+    function setOption(string memory option)
     	public
     {
     	// TODO change to depend on access rights and can only be called at the start
     	if(true) {
-            Graph storage graph = graphs[1];
-    	    graph.issue = issue;
+            Weighing storage weighing = weighings[1];
+    	    weighing.option = option;
     	}
     }
     
@@ -55,7 +55,7 @@ contract Balancing {
 
     function voteOnReason
     (
-        string memory justification, 
+        string memory ground, 
         // string memory issue, 
         string memory polarity, 
         uint256 confidence
@@ -63,43 +63,39 @@ contract Balancing {
         public
         returns (int256 re)
     {
-        Graph storage graph = graphs[1];
+        Weighing storage weighing = weighings[1];
 
         re = 0;
         // check that reason is not known yet.
-        for (uint256 j = 1; j < graph.reasonsIDs.count() + 1; j++) {
-            uint256 reasonId = uint256(reasonsIds.keyAtIndex(j));
-            if (compareStrings(graph.reasons[reasonId].justification, justification) && 
+        for (uint256 j = 1; j < weighing.reasonsIDs.count() + 1; j++) {
+            uint256 reasonID = uint256(reasonsIDs.keyAtIndex(j));
+            if (compareStrings(weighing.reasons[reasonID].ground, ground) && 
             // compareStrings(graph.reasons[j].issue, issue) && 
-            compareStrings(graph.reasons[reasonId].polarity, polarity)) {
+            compareStrings(weighing.reasons[reasonID].polarity, polarity)) {
                 // conclude reason is already in reasons
                 // increase weight by magnitude
-                graph.reasons[reasonId].weight = graph.reasons[reasonId].weight + confidence*reputations[msg.sender];
+                weighing.reasons[reasonID].weight = weighing.reasons[reasonID].weight + confidence*reputations[msg.sender];//maybe this should be magnitude instead of conf*rep
 
-                HitchensUnorderedKeySetLib.Set storage source = sources[
-                    msg.sender
-                ];
-                source.insert(bytes32(reasonId));
-                re = int256(reasonId);
+                HitchensUnorderedKeySetLib.Set storage source = sources[ msg.sender];
+                source.insert(bytes32(reasonID));
+                re = int256(reasonID);
                 break;
             }
         }
 
         if (re == 0) {// reason is new add reason and set weight to 1
-            uint256 reasonID = graph.reasonsIDs.count() + 1;
-            graph.reasonsIDs.insert(bytes32(reasonID));
-            Reason storage reason = graph.reasons[reasonID];
-            // reason.justification = justification;
-            graph.reasons[reasonID].justification = justification;
-            // reason.issue = issue;
+            uint256 reasonID = weighing.reasonsIDs.count() + 1;
+            weighing.reasonsIDs.insert(bytes32(reasonID));
+            Reason storage reason = weighing.reasons[reasonID];
+            // reason.ground = ground;
+            weighing.reasons[reasonID].ground = ground;
+            // reason.option = option;
             // reason.polarity = polarity;
-            graph.reasons[reasonID].polarity = polarity;
+            weighing.reasons[reasonID].polarity = polarity;
             // weights[reasonID-1] = confidence*reputations[msg.sender];
-            graph.reasons[reasonID].weight = graph.reasons[reasonID].weight + confidence*reputations[msg.sender];
+            weighing.reasons[reasonID].weight = weighing.reasons[reasonID].weight + confidence*reputations[msg.sender];// maybe this should be magnitude instead of conf*rep
     
-            HitchensUnorderedKeySetLib.Set storage source = sources[
-                msg.sender
-            ];
+            HitchensUnorderedKeySetLib.Set storage source = sources[ msg.sender];
             source.insert(bytes32(reasonID));
             re = int256(reasonID);
         }
@@ -140,7 +136,7 @@ contract Balancing {
         // view 
         returns (string memory dec)
     {
-        Graph storage graph = graphs[1];
+        Weighing storage weighing = weighings[1];
 
         dec = '';
         uint256 magn = 0;
@@ -151,32 +147,30 @@ contract Balancing {
         int256 sum = 0;
         uint256 neut = 0;
         string memory cs = '';
-        uint256 rs = graph.reasonsIDs.count();   
+        uint256 rs = weighing.reasonsIDs.count();   
 
 
         for (uint256 i = 0; i < rs; i++){
                 
-            uint256 reasonID = uint256(graph.reasonsIDs.keyAtIndex(i));
-            Reason storage reason = graph.reasons[reasonId];//not sure if this works
+            uint256 reasonID = uint256(weighing.reasonsIDs.keyAtIndex(i));
+            Reason storage reason = weighing.reasons[reasonID];//not sure if this works
             
-            cs = string(abi.encodePacked(cs, " ", i, " ", reason.justification));
+            cs = string(abi.encodePacked(cs, " ", i, " ", reason.ground));
 
-            if (compareStrings(reason.issue, issueTBD)) {
-                if (compareStrings(reason.polarity, '0')) { 
-                    neut += uint256(reason.weight);
-                }
-                else if (compareStrings(reason.polarity, '-')) { 
-                    neg += uint256(reason.weight);
-                }
-                else if (compareStrings(reason.polarity, '+')) { 
-                    pos += uint256(reason.weight);
-                }
-                else if (compareStrings(reason.polarity, '--')) { 
-                    nneg += uint256(reason.weight);
-                }
-                else if (compareStrings(reason.polarity, '++')) { 
-                    ppos += uint256(reason.weight);
-                }
+            if (compareStrings(reason.polarity, '0')) { 
+                neut += uint256(reason.weight);
+            }
+            else if (compareStrings(reason.polarity, '-')) { 
+                neg += uint256(reason.weight);
+            }
+            else if (compareStrings(reason.polarity, '+')) { 
+                pos += uint256(reason.weight);
+            }
+            else if (compareStrings(reason.polarity, '--')) { 
+                nneg += uint256(reason.weight);
+            }
+            else if (compareStrings(reason.polarity, '++')) { 
+                ppos += uint256(reason.weight);
             }
         }  
 
@@ -215,8 +209,8 @@ contract Balancing {
         }
 
 
-        graph.decision = dec;
-        emit Output(graph);
+        weighing.decision = dec;
+        emit Output(weighing);
 
 
         return dec;
